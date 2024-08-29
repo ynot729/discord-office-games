29f3517595d1b8af6cef4dc15bdd0318ae684ea3
Raul Reyes <raul@reclique.com>
2024-08-29 06:53:54 -0400

    configure prettier

--- app.ts
+++ app.ts
@@ -1,28 +1,20 @@
-import { Client, GatewayIntentBits, TextChannel } from "discord.js";
-import { generateSlug } from "random-word-slugs";
+import { Client, GatewayIntentBits, TextChannel } from 'discord.js';
+import { generateSlug } from 'random-word-slugs';
 
-const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildModeration,
-		GatewayIntentBits.GuildMessages,
-	],
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildMessages] });
 
 const token = process.env.DISCORD_TOKEN;
 const channelId = process.env.CHANNEL_ID;
-const intervalMinutes = process.env.INTERVAL_MINUTES
-	? parseInt(process.env.INTERVAL_MINUTES)
-	: 5;
+const intervalMinutes = process.env.INTERVAL_MINUTES ? parseInt(process.env.INTERVAL_MINUTES) : 5;
 
-if (!token) throw new Error("ENV needs to have DISCORD_TOKEN");
-if (!channelId) throw new Error("ENV needs to have CHANNEL_ID");
+if (!token) throw new Error('ENV needs to have DISCORD_TOKEN');
+if (!channelId) throw new Error('ENV needs to have CHANNEL_ID');
 
 let channel: TextChannel;
-client.once("ready", async () => {
-	console.log("Bot is online!");
+client.once('ready', async () => {
+	console.log('Bot is online!');
 	channel = client.channels.cache.get(channelId) as TextChannel;
-	if (!channel) return console.error("Channel not found!");
+	if (!channel) return console.error('Channel not found!');
 	await loadStanding();
 	// await channel.send("Hello, world!");
 	// sendRandomMessage();
@@ -42,14 +34,14 @@ const questions: (() => Question)[] = [
 		return { q: `What is \`${a} + ${b}\`?`, a: `${a + b}` };
 	},
 	() => {
-		const thingToSay = generateSlug(3, { format: "sentence" });
+		const thingToSay = generateSlug(3, { format: 'sentence' });
 		return {
 			q: `Say: \`${thingToSay.toLowerCase()}\` in uppercase`,
 			a: thingToSay.toUpperCase(),
 		};
 	},
 	() => {
-		const thingToSay = generateSlug(3, { format: "sentence" });
+		const thingToSay = generateSlug(3, { format: 'sentence' });
 		return {
 			q: `Say: \`${thingToSay.toUpperCase()}\` in lowercase`,
 			a: thingToSay.toLowerCase(),
@@ -61,16 +53,16 @@ const standings: Record<string, number> = {};
 async function loadStanding() {
 	try {
 		//@ts-ignore the file might not be there on first run
-		const loadedStandings = await import("./standings.json");
+		const loadedStandings = await import('./standings.json');
 		Object.assign(standings, loadedStandings.default);
-		console.log("Standings loaded!");
+		console.log('Standings loaded!');
 		console.log(standings);
 	} catch (error) {
-		console.error("No standings found");
+		console.error('No standings found');
 	}
 }
 
-client.on("messageCreate", async (message) => {
+client.on('messageCreate', async (message) => {
 	if (message.channel.id === channelId) {
 		const msgDtls = await message.fetch();
 		const providedAnswer = msgDtls.content;
@@ -78,7 +70,7 @@ client.on("messageCreate", async (message) => {
 		const name = msgDtls.member?.displayName ?? msgDtls.author.displayName;
 		// console.log(`Message from: ${name}`);
 
-		if (name == "office games") return; // no need to respond when we sent the message
+		if (name == 'office games') return; // no need to respond when we sent the message
 
 		if (currentQuestion) {
 			if (currentQuestion.a === providedAnswer) {
@@ -91,27 +83,25 @@ client.on("messageCreate", async (message) => {
 
 				currentQuestion = null;
 
-				msgDtls.react("✅");
+				msgDtls.react('✅');
 				saveStandings();
 			} else {
-				msgDtls.react("❌");
-				console.log(
-					`Inorrect answer '${providedAnswer}' != '${currentQuestion.a}' from: ${name}`
-				);
+				msgDtls.react('❌');
+				console.log(`Inorrect answer '${providedAnswer}' != '${currentQuestion.a}' from: ${name}`);
 			}
 		}
 
-		if (providedAnswer == "standings") {
+		if (providedAnswer == 'standings') {
 			const standingsMsg = Object.entries(standings)
 				.map(([name, score]) => `${name}: ${score}`)
-				.join("\n");
-			channel.send("```\n" + standingsMsg + "\n```");
+				.join('\n');
+			channel.send('```\n' + standingsMsg + '\n```');
 		}
 	}
 });
 
 function askQuestion() {
-	console.log("Asking question ...");
+	console.log('Asking question ...');
 	const questionN = Math.floor(Math.random() * questions.length);
 	const processingQuestion = questions[questionN];
 	const question_parts = processingQuestion();
@@ -122,9 +112,9 @@ function askQuestion() {
 }
 
 async function saveStandings() {
-	console.log("Saving standings ...");
-	const fs = await import("fs");
-	fs.writeFileSync("./standings.json", JSON.stringify(standings));
+	console.log('Saving standings ...');
+	const fs = await import('fs');
+	fs.writeFileSync('./standings.json', JSON.stringify(standings));
 }
 
 client.login(token);
