28bd2fe865910224d2a721437ae0684033cc386b
Raul Reyes <ynot729@users.noreply.github.com>
2024-09-25 07:02:31 -0400

    day standings show each hour

--- standings.ts
+++ standings.ts
@@ -1,10 +1,13 @@
 import { TextChannel } from 'discord.js';
 import { db } from './db';
+import { markdownTable } from 'markdown-table';
+import { DateTime } from 'luxon';
 
 export function addPoint(channel: TextChannel, name: string, question: Question) {
-	let query = db.query('INSERT INTO points (id,channelID,userName,category) VALUES ($id,$channelID,$userName,$category)');
+	let query = db.query('INSERT INTO points (id,createdAt,channelID,userName,category) VALUES ($id,$createdAt,$channelID,$userName,$category)');
 	query.run({
 		$id: null,
+		$createdAt: DateTime.now().toSQL({ includeOffset: false }),
 		$channelID: channel.id,
 		$userName: name,
 		$category: question.category,
@@ -19,17 +22,25 @@ export function getStandings(channel: TextChannel, type = 'all') {
 	for (const requestedType of requestedTypes) {
 		switch (requestedType) {
 			case 'day': {
-				let query = db.query(`SELECT userName,count(id) as Points
+				let hoursQuery = db.query(`select strftime('%H',createdAt) as Hour
+					from points
+					WHERE channelID = $channelID AND createdAt > datetime('now', 'start of day')
+					group by Hour`);
+				let hours = hoursQuery.all({ $channelID: channel.id }) as { Hour: string }[];
+				let hoursSelects = hours.map((h) => `count(case when strftime('%H',createdAt)='${h.Hour}' then id else null end) as '${h.Hour}'`).join(',\n'); // -4 because of timezone
+				let query = db.query(`SELECT userName
+							, ${hoursSelects}
+							, count(id) as Points
 						FROM points
 						WHERE channelID = $channelID AND createdAt > datetime('now', 'start of day')
 						GROUP BY userName
 						ORDER BY Points DESC
 						`);
 				let results = query.all({ $channelID: channel.id }) as { userName: string; Points: number }[];
-				message.push('Day:');
-				results.forEach((result) => {
-					message.push(`\t${result.userName}: ${result.Points}`);
-				});
+				if (results.length > 0) {
+					message.push('Day:');
+					message.push(resultsToTable(results));
+				}
 				break;
 			}
 			case 'week': {
@@ -64,4 +75,10 @@ export function getStandings(channel: TextChannel, type = 'all') {
 		message.push('');
 	}
 	return message.join('\n').trim();
+}
+
+function resultsToTable(data: Record<string, string | number>[], title = '') {
+	const table = [Object.keys(data[0]), ...data.map((r) => Object.values(r) as string[])];
+	if (title) table[0][0] = title;
+	return markdownTable(table);
 }
